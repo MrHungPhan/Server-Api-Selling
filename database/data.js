9f5efcbe75956535d94c5e67d3040ceb34ed74b6
mrhungphan <hungphan5995@gmail.com>
2019-05-23 17:23:09 +0700

    test mysql new

--- database/data.js
+++ database/data.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = {};
-const sequelize = new Sequelize("banhang", "root", "", {
-    host : 'localhost',
+const sequelize = new Sequelize("sql12292961", "sql12292961", "jQMhTdJ3LD", {
+    host : 'sql12.freemysqlhosting.net',
     dialect : 'mysql',
 
     pool : {
