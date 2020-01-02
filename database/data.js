42edfd96a8cbf2179d4fdf8284c25c1de80812a8
phan.van.hung <phan.van.hung@b122039-mb.local>
2020-01-02 15:04:55 +0700

    change server database

--- database/data.js
+++ database/data.js
@@ -1,20 +1,8 @@
 const Sequelize = require('sequelize');
 const db = {};
 // ban hang, root, '', localhost.
-// const sequelize = new Sequelize("sql12292961" , "sql12292961", "jQMhTdJ3LD", {
-//     host : 'sql12.freemysqlhosting.net',
-//     dialect : 'mysql',
-
-//     pool : {
-//         max : 5,
-//         min : 0,
-//         acquire : 30000,
-//         idle : 10000
-//     }
-// })
-
-const sequelize = new Sequelize("banhang" , "root", "", {
-    host : 'localhost',
+const sequelize = new Sequelize("sql12292961" , "sql12292961", "jQMhTdJ3LD", {
+    host : 'sql12.freemysqlhosting.net',
     dialect : 'mysql',
 
     pool : {
@@ -24,6 +12,18 @@ const sequelize = new Sequelize("banhang" , "root", "", {
         idle : 10000
     }
 })
+
+// const sequelize = new Sequelize("banhang" , "root", "", {
+//     host : 'localhost',
+//     dialect : 'mysql',
+
+//     pool : {
+//         max : 5,
+//         min : 0,
+//         acquire : 30000,
+//         idle : 10000
+//     }
+// })
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
